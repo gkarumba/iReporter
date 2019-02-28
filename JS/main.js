7e29d94107d483dc2e2290a053ef385521c44c9f
gkarumba <gachegua@gmail.com>
2019-02-28 17:27:51 +0300

    Adds the login user functionality

--- JS/main.js
+++ JS/main.js
@@ -1,4 +1,5 @@
 const registerBtn = document.getElementById("registerButton");
+const loginBtn = document.getElementById("loginButton");
 
 function registerUser(){
     let output;
@@ -35,6 +36,69 @@ function registerUser(){
     });
 }
 
+function decodeJwt (token){
+    let base64Url = token.split('.')[1];
+    let base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    return JSON.parse(window.atob(base64));
+}
+
+function logInResponse(){
+    let response = sessionStorage.getItem("logInResponse");
+    let output = `<p style="background: #004e00;color: white;text-align: center;padding: 20px;font-family: 'Boogaloo', cursive;" >${response}</p>`;
+    return document.getElementById("redirectedLogIn").innerHTML = output;
+}
+
+function loginUser(){
+    let output;
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
+
+    fetch('https://i-reporter-v2-gkarumba.herokuapp.com/users/v2/login',{
+        mode:'cors',
+        method:'POST',
+        headers:{
+            Accept:'application/json',
+            'Content-Type':'application/json;charset=UTF-8'
+        },
+        body:JSON.stringify({
+            email:email,password:password
+        })
+    })
+    .then((res) => {
+        if (res.ok){
+            return res.json().then((myJson) => {
+                sessionStorage.setItem("token", myJson.token);
+                sessionStorage.setItem("logInResponse",myJson.message);
+
+                let token = sessionStorage.getItem("token").replace("'","");
+                token = token.substr(0, token.length-1);
+                let decodeToken = decodeJwt(token);
+                decodeTokenId = Object.values(decodeToken)[2];
+                if(decodeTokenId === 1){
+                    redirect: window.location.assign("./admin.html");
+                }else{
+                    redirect: window.location.assign("./profile.html");
+                }
+            });
+        }
+        if (res.status == 400){
+            return res.json().then((myJson) => {
+                output = `<p style="background: #004e00;color: white;text-align: center;padding: 20px;font-family: 'Boogaloo', cursive;" >${myJson.message}</p>`;
+                return document.getElementById("loginResponse").innerHTML = output;
+            });
+        }
+        if (res.status == 401){
+            return res.json().then((myJson) => {
+                output = `<p style="background: #004e00;color: white;text-align: center;padding: 20px;font-family: 'Boogaloo', cursive;" >${myJson.message}</p>`;
+                return document.getElementById("loginResponse").innerHTML = output;
+            });
+        }
+    });
+}
+
 if(registerBtn){
     registerBtn.addEventListener('click', registerUser)
+}
+if(loginBtn){
+    loginBtn.addEventListener('click', loginUser)
 }
